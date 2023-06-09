6b601ae821b88a1b5d836a233f33eaee007d1644
saiyck <saiyckpm@gmail.com>
2023-06-10 02:19:16 +0530

    aoikey

--- src/Common.js
+++ src/Common.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+
 export const handleUpload = (newfiles) => {
     let formData = new FormData();
     formData.append("file", newfiles);
     formData.append("model", "whisper-1");
+    console.log(process.env.WHISPER_API_KEY,'keyyyyyy');
     return new Promise((resolve,reject)=> {
         axios({
             url:'https://api.openai.com/v1/audio/transcriptions',
             method:'POST',
             headers:{
-                Authorization: 'Bearer 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'
+                Authorization: `Bearer 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`
             },
             data: formData
         }).then((res)=> {
